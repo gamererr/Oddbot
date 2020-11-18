d48db992f0b7419331f2f335fcdd2e638947a689
gamererr <55727914+gamererr@users.noreply.github.com>
2020-11-18 12:50:15 -0500

    renamed !nuts to !gotem

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def on_ready():
 prefix = "!"
 
 # lists
-nuts = ["balls", "nuts", "mama", "balls", "nuts", "deez nuts", "mama", "this cock", "my face", "penis"]
+nuts = ["balls", "nuts", "deez nuts", "mama", "this cock", "my face", "penis", "dickenballs", "cockandnutz"]
 ontop = ["oddspray", "gsg"]
 
 # embeds
@@ -41,10 +41,12 @@ async def on_message(message):
     if message.content.startswith(prefix):
         if (argslist[1] == "help"):
             await message.channel.send("there are no commands")
-        elif (argslist[1] == "nuts"):
+        elif (argslist[1] == "gotem"):
             await message.channel.send(f"{message.content[5:]} {nuts[random.randrange(len(nuts))]}")
         elif (argslist[1] == "ontop"):
             await message.channel.send(f"{ontop[random.randrange(len(ontop))]} > {message.guild.members[random.randrange(len(message.guild.members))].name}")
+        elif (argslist[1] == "jokestar"):
+            return
         else:
             await message.channel.send(f"thats not a command, use {prefix}help for a list of commands")
         
