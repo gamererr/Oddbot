405eeb27e5b20693f705ef545983cf822c09e983
gamererr <55727914+gamererr@users.noreply.github.com>
2020-11-18 13:04:28 -0500

    added help message

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ nuts = ["balls", "nuts", "deez nuts", "mama", "this cock", "my face", "penis", "
 ontop = ["oddspray", "gsg"]
 
 # embeds
+helpmessage = discord.Embed(title="Commands", description=f'**{prefix}gotem** - sends everything after {prefix}gotem then a random item from a list of genitalia related words"\n**{prefix}ontop** sends "oddspray/gs > a random user in the server"', color=0x7e01e4)
 
 
 @client.event
@@ -40,14 +41,16 @@ async def on_message(message):
     
     if message.content.startswith(prefix):
         if (argslist[1] == "help"):
-            await message.channel.send("there are no commands")
+            helpmessage.color = 0x00C400
+            await message.channel.send(embed=helpmessage)
         elif (argslist[1] == "gotem"):
-            await message.channel.send(f"{message.content[5:]} {nuts[random.randrange(len(nuts))]}")
+            await message.channel.send(f"{message.content[6:]} {nuts[random.randrange(len(nuts))]}")
         elif (argslist[1] == "ontop"):
             await message.channel.send(f"{ontop[random.randrange(len(ontop))]} > {message.guild.members[random.randrange(len(message.guild.members))].name}")
         elif (argslist[1] == "jokestar"):
             return
         else:
-            await message.channel.send(f"thats not a command, use {prefix}help for a list of commands")
+            helpmessage.color = 0xFF0000
+            await message.channel.send("unknown command, here is a list of commands", embed=helpmessage)
         
 client.run(token)
